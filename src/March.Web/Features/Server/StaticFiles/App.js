9e0f36ba348f676d1829b9d3366bf33d5c96fa9e
Saku Laakso <saku.laakso@solita.fi>
2024-11-11 10:17:25 +0200

    Refactor app default route logic

--- src/March.Web/Features/Server/StaticFiles/App.js
+++ src/March.Web/Features/Server/StaticFiles/App.js
@@ -2,21 +2,6 @@
     return window.matchMedia("(max-width: 768px)").matches;
 }
 
-function getRouteContentsFromServer(evt) {
-    const route = window.location.pathname !== "/" ? window.location.pathname : "/home";
-    const targetElement = document.getElementById('mainContentArea');
-    if (targetElement) {
-        htmx.ajax('GET', route, {
-            target: targetElement,
-            swap: 'innerHTML',
-            error: (err) => console.error('Failed to load content:', err)
-        });
-    }
-    document.body.removeEventListener('htmx:afterSwap', getRouteContentsFromServer);
-}
-
-document.body.addEventListener('htmx:afterSwap', getRouteContentsFromServer);
-
 document.body.addEventListener('htmx:responseError', function (event) {
     document.getElementById('error').innerHTML = event.detail.xhr.response;
 });
